4fd6cad6ae1cb49923c5608e220a69b22315a48a
mageichler <mageichler@gmail.com>
2020-03-09 14:43:45 +0100

    Dinosaurus check

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <?php include_once 'res/head.html'; 
+  echo "ss";
+  ?>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Magdalena Eichler – portfolio</a>
+    </nav>
+
+    <main role="main" class="container">
+
+      <h1>Hello there!</h1>
+
+    </main>
+    <!-- /.container -->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/jquery.slim.min.js"><\/script>')</script><script src="js/bootstrap.bundle.min.js"></script></body>
+</html>
