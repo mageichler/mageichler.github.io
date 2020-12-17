c76c50b21f2386e410e97cd2b94de5e12f43d225
mageichler <mageichler@gmail.com>
2020-12-17 17:51:15 +0100

    Finished page about programming school page

---
+++ js/mageichler.js
@@ -0,0 +1,12 @@
+var password;
+
+var password_correct="Witch";
+
+password=prompt("Please, use the password to enter the page I've sent you by email. If you don't have the password and browsing my page because of recruitment process / potentail job, just email me. Thank you!",' ');
+
+if (password==password_correct) {
+  
+  }
+else {
+    window.location="you-shell-not-pass.html";
+  }
